022d9228f7e25a463c5396e544159db62d9e152b
Ray Creager <ray@elrohir.(none)>
2013-10-08 22:01:36 -0400

    Added changes to vegas_spec-hpc.bash to allow running on local machine in simulation mode.

--- src/vegas_hpc/vegas_spec-hpc.bash
+++ src/vegas_hpc/vegas_spec-hpc.bash
@@ -9,15 +9,15 @@ export VEGAS_DIR=$VEGAS/vegas_hpc
 
 export CUDA=/opt/local/cuda50
 
-export PYSLALIB=/home/gbt7/newt/lib/python2.7/site-packages/pyslalib
+export PYSLALIB=/export/home/elrohir/rcreager/rc/newt/lib/python2.7/site-packages/pyslalib
 
 export VEGAS_INCL=/opt/local/include
 #export VEGAS_BIN=/opt/local/bin
-export VEGAS_LIB=/home/gbt7/newt/lib
+export VEGAS_LIB=/export/home/elrohir/rcreager/rc/newt/lib
 export VEGAS_LIB_GCC=/usr/lib/gcc/x86_64-redhat-linux/3.4.6
 
 export PATH=$VEGAS_DIR/bin:$CUDA/bin:$VEGAS_BIN:$PATH
 
-export PYTHONPATH=$VEGAS/lib/python/site-packages:$VEGAS/lib/python:$VEGAS_DIR/python:/home/gbt7/newt/lib/pythong2.7/site-packages:$PYTHONPATH
+export PYTHONPATH=$VEGAS/lib/python/site-packages:$VEGAS/lib/python:$VEGAS_DIR/python:/export/home/elrohir/rcreager/rc/newt/lib/python2.7/site-packages:$PYTHONPATH
 
 export LD_LIBRARY_PATH=$PYSLALIB:$VEGAS_LIB:$CUDA/lib64:$LD_LIBRARY_PATH
