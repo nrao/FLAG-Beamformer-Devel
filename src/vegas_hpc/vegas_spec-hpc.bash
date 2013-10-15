6ffacf947491a2d9dd6da5829dce2669aed4a8cc
Ramon Creager <rcreager@nrao.edu>
2013-10-15 11:35:38 +0800

    Added vegas_spec-hpc.bash back in. Deleted by accident upstream.

---
+++ src/vegas_hpc/vegas_spec-hpc.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Set environment variables for VEGAS, bash version
+echo "This script is specific to spec-hpc-xx"
+echo "Setting VEGAS_DIR, CUDA, PYSLALIB, VEGAS_INCL/BIN/LIB, PATH, PYTHONPATH and LD_LIBRARY_PATH for VEGAS..."
+
+# Note: user must set the VEGAS variable in their bash startup script
+
+export VEGAS_DIR=$VEGAS/vegas_hpc
+
+export CUDA=/opt/local/cuda50
+
+export PYSLALIB=/home/gbt7/newt/lib/python2.7/site-packages/pyslalib
+
+export VEGAS_INCL=/opt/local/include
+#export VEGAS_BIN=/opt/local/bin
+export VEGAS_LIB=/home/gbt7/newt/lib
+export VEGAS_LIB_GCC=/usr/lib/gcc/x86_64-redhat-linux/3.4.6
+
+export PATH=$VEGAS_DIR/bin:$CUDA/bin:$VEGAS_BIN:$PATH
+
+export PYTHONPATH=$VEGAS/lib/python/site-packages:$VEGAS/lib/python:$VEGAS_DIR/python:/home/gbt7/newt/lib/pythong2.7/site-packages:$PYTHONPATH
+
+export LD_LIBRARY_PATH=$PYSLALIB:$VEGAS_LIB:$CUDA/lib64:$LD_LIBRARY_PATH
