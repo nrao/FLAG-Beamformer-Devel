2bddf3242b018e374a49e372fcd8eb7a8a0e4da7
Joe Brandt <jbrandt@nrao.edu>
2013-10-30 14:41:18 -0400

    Restored NRAO configuration of vegas_spec-hpc.bash and HPC Makefile

--- src/vegas_hpc/vegas_spec-hpc.bash
+++ src/vegas_hpc/vegas_spec-hpc.bash
@@ -7,17 +7,17 @@ echo "Setting VEGAS_DIR, CUDA, PYSLALIB, VEGAS_INCL/BIN/LIB, PATH, PYTHONPATH an
 
 export VEGAS_DIR=$VEGAS/vegas_hpc
 
-export CUDA=/usr/local/cuda
+export CUDA=/opt/local/cuda50
 
-export PYSLALIB=/opt/dibas/pulsar/lib/python2.6/site-packages/pyslalib/slalib.so
+export PYSLALIB=/home/gbt7/newt/lib/python2.7/site-packages/pyslalib
 
-export VEGAS_INCL=/opt/dibas/dibaslibs/include
-export VEGAS_BIN=/opt/dibas/dibaslibs/bin
-export VEGAS_LIB=/opt/dibas/dibaslibs/lib
-#export VEGAS_LIB_GCC=/usr/lib/gcc/x86_64-redhat-linux/3.4.6
+export VEGAS_INCL=/opt/local/include
+#export VEGAS_BIN=/opt/local/bin
+export VEGAS_LIB=/home/gbt7/newt/lib
+export VEGAS_LIB_GCC=/usr/lib/gcc/x86_64-redhat-linux/3.4.6
 
 export PATH=$VEGAS_DIR/bin:$CUDA/bin:$VEGAS_BIN:$PATH
 
-export PYTHONPATH=$VEGAS/lib/python/site-packages:$VEGAS/lib/python:$VEGAS_DIR/python:/opt/dibas/dibaslibs/lib/python2.6/site-packages:$PYTHONPATH
+export PYTHONPATH=$VEGAS/lib/python/site-packages:$VEGAS/lib/python:$VEGAS_DIR/python:/home/gbt7/newt/lib/pythong2.7/site-packages:$PYTHONPATH
 
 export LD_LIBRARY_PATH=$PYSLALIB:$VEGAS_LIB:$CUDA/lib64:$LD_LIBRARY_PATH
