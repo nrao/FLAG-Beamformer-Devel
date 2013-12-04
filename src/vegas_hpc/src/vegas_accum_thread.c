50d8bb14d2b318e3106e4ca38dd137164f10579a
Joe Brandt <jbrandt@nrao.edu>
2013-12-04 14:50:03 -0500

    Changes to use calculated spectra per exposure to control when exposures are partitioned/labeled for the manager.

--- src/vegas_hpc/src/vegas_accum_thread.c
+++ src/vegas_hpc/src/vegas_accum_thread.c
@@ -40,12 +40,20 @@
 #define BLANKING_MASK   (0x8)
 #define CAL_SR_MASK     (0x7)
 
+int g_debug_accumulator_thread = 0; // 
+
 struct BlockStats
 {
     int nblock_int;
     int npacket;
     int n_pkt_drop;
     int n_heap_drop;
+};
+
+struct Clock
+{
+    uint64_t last_time_counter;
+    uint64_t upper_bits;
 };
 
 // Read a status buffer all of the key observation paramters
@@ -58,14 +66,17 @@ extern void vegas_read_subint_params(char *buf,
                                      struct vegas_params *g,
                                      struct sdfits *p);
                                      
-extern int check_scan_length(int64_t time_counter, double fpgafreq, double scanlen);
+int check_scan_length(struct Clock *clock, int64_t time_counter, double fpgafreq, double scanlen);
+
+uint64_t get_full_range_counter(struct Clock *clock, uint64_t raw_time_counter);
 
 void write_full_integration(struct vegas_databuf *db_out, int *cur_block_out, 
                        struct vegas_databuf *db_in,  int  cur_block_in, 
                        char *accum_dirty, float **accumulator, 
                        struct sdfits_data_columns* data_cols,
                        struct sdfits *sdf,struct BlockStats *blkstat);
-void flush_end_of_scan(struct vegas_databuf *db_out, int *cur_block_out);                       
+void flush_end_of_scan(struct vegas_databuf *db_out, int *cur_block_out, 
+                       struct vegas_databuf *db_in, int cur_block_in);                       
 
 /* Dynamically allocates memory for the vector accumulators */
 void create_accumulators(float ***accumulator, int num_chans, int num_subbands)
@@ -151,6 +162,9 @@ void vegas_accum_thread(void *_args) {
     double fpgafreq;
     double scan_length_seconds;
     int accumid_xor_mask = 0;
+    uint64_t end_exposure_spectrum_number;
+    uint64_t spectra_per_exposure;
+    struct Clock clock;
 
 
     /* Get arguments */
@@ -175,6 +189,10 @@ void vegas_accum_thread(void *_args) {
         vegas_error("vegas_accum_thread", "Error setting priority level.");
         perror("set_priority");
     }
+    
+    // Reset the 64bit emulation of a 40 bit counter
+    clock.upper_bits = 0;
+    clock.last_time_counter = 0;    
 
     /* Attach to status shared mem area */
     struct vegas_status st;
@@ -222,6 +240,7 @@ void vegas_accum_thread(void *_args) {
 
     /* Determine high/low bandwidth mode */
     char bw_mode[16];
+    vegas_status_lock_safe(&st);
     if (hgets(st.buf, "BW_MODE", 16, bw_mode))
     {
         if(strncmp(bw_mode, "high", 4) == 0)
@@ -229,26 +248,36 @@ void vegas_accum_thread(void *_args) {
         else if(strncmp(bw_mode, "low", 3) == 0)
             payload_type = FLOAT_PAYLOAD;
         else
-            vegas_error("vegas_net_thread", "Unsupported bandwidth mode");
+            vegas_error("vegas_accum_thread", "Unsupported bandwidth mode");
     }
     else
-        vegas_error("vegas_net_thread", "BW_MODE not set");
+        vegas_error("vegas_accum_thread", "BW_MODE not set");
+    vegas_status_unlock_safe(&st);
 
     /* Read nchan and nsubband from status shared memory */
     vegas_read_obs_params(st.buf, &gp, &sf);
     use_scanlen = read_scan_length(st.buf, &fpgafreq, &scan_length_seconds);
-
+    
+    vegas_status_lock_safe(&st);
     /* read switching signal inversion mask, if present */
     if (hgeti4(st.buf, "_SWSGPLY", &accumid_xor_mask) == 0)
     {
-        // switching signals used as is
+        // switching signals are used as is
         accumid_xor_mask = 0x0;
     }
-    
+    vegas_status_unlock_safe(&st);
 
     /* Allocate memory for vector accumulators */
     create_accumulators(&accumulator, sf.hdr.nchan, sf.hdr.nsubband);
-    pthread_cleanup_push((void *)destroy_accumulators, accumulator);
+    pthread_cleanup_push((void *)destroy_accumulators, accumulator);    
+    spectra_per_exposure = sf.data_columns.exposure/sf.hdr.hwexposr;
+    
+    if (spectra_per_exposure < 2)
+    {
+        vegas_error("vegas_accum thread", "exposure/hwposer is too small exiting...");
+        pthread_exit(0);
+    }
+    end_exposure_spectrum_number = spectra_per_exposure;
 
     /* Clear the vector accumulators */
     for(i = 0; i < NUM_SW_STATES; i++) accum_dirty[i] = 1;
@@ -261,15 +290,16 @@ void vegas_accum_thread(void *_args) {
     double accum_time=0;
     int integ_num = 0;
     float pfb_rate = 0.0;
-    int heap, accumid, struct_offset, array_offset;
+    int heap, accumid;
+    // int struct_offset, array_offset;
     char *hdr_in=NULL, *hdr_out=NULL;
     struct databuf_index *index_in, *index_out;
-    uint64_t scan_length_time_counter;
-    uint64_t raw_time_counter=0;
+    uint64_t full_time_counter=0;
     struct BlockStats blkstats;
+    int do_once=1;
 
     // int nblock_int=0, npacket=0, n_pkt_drop=0, n_heap_drop=0;
-    int debug_tick = 0;
+    // int debug_tick = 0;
 
     signal(SIGINT,cc);
     while (run) {
@@ -298,7 +328,7 @@ void vegas_accum_thread(void *_args) {
 
         /* Do any first time stuff: first time code runs, not first time process this block */
         if (first) {
-
+        
             /* Set up first output header. This header is copied from block to block
                each time a new block is created */
             hdr_out = vegas_databuf_header(db_out, curblock_out);
@@ -313,15 +343,13 @@ void vegas_accum_thread(void *_args) {
             index_out = (struct databuf_index*)vegas_databuf_index(db_out, curblock_out);
             index_out->num_datasets = 0;
             index_out->array_size = sf.hdr.nsubband * sf.hdr.nchan * NUM_STOKES * 4;
-            
-            scan_length_time_counter = 0;
-            first=0;
-            check_scan_length(0,0,0); // resets function static local variables
 
             blkstats.nblock_int = 0;
             blkstats.npacket = 0;
             blkstats.n_pkt_drop = 0;
             blkstats.n_heap_drop = 0;
+                                    
+            first=0;
         }
 
         /* Loop through each spectrum (heap) in input buffer */
@@ -356,20 +384,32 @@ void vegas_accum_thread(void *_args) {
              */
             freq_heap->status_bits = (freq_heap->status_bits ^ accumid_xor_mask) & (CAL_SR_MASK|BLANKING_MASK);
             /* for accumid we only care for the lower bits */
-            accumid = freq_heap->status_bits & CAL_SR_MASK;         
-
-            /*Debug: print heap */
-#if 0
-            printf("FH: %d, %d, %d, %d, %d, %d %f %f %d\n", freq_heap->time_cntr, freq_heap->spectrum_cntr,
-                freq_heap->integ_size, freq_heap->mode, freq_heap->status_bits,
-                freq_heap->payload_data_off, accum_time, reqd_exposure, integ_num);
-#endif
+            accumid = freq_heap->status_bits & CAL_SR_MASK;
+                                 
             // calculate the 40 bit raw time counter
-            raw_time_counter = (((uint64_t)freq_heap->time_cntr_top8) << 32)
-                                     + (uint64_t)freq_heap->time_cntr;
-            
+            full_time_counter = get_full_range_counter(&clock, (((uint64_t)freq_heap->time_cntr_top8) << 32)
+                                                       + (uint64_t)freq_heap->time_cntr);
+
+            if (do_once)
+            {
+                uint64_t nspec_by_accumtime = pfb_rate/(float)(freq_heap->integ_size);
+                printf("\n\nNSPECT by accumtime = %ld, \nNSPECT by hwexp = %ld\n", nspec_by_accumtime, spectra_per_exposure);
+                printf("NSPECT by acclen,pfbrate and exposure: %f\n", pfb_rate/freq_heap->integ_size*reqd_exposure);
+                printf("pfbrate=%f, integsize=%d\n", pfb_rate, freq_heap->integ_size);
+                do_once=0;
+            }            
+            /*Debug: print heap */
+            if (g_debug_accumulator_thread)
+            {
+                printf("FH: %lu, %lu, %d, %d, %d, %d, %d %f %f %d\n", full_time_counter, 
+                    (((uint64_t)freq_heap->time_cntr_top8) << 32) + (uint64_t)freq_heap->time_cntr,
+                    freq_heap->spectrum_cntr,
+                    freq_heap->integ_size, freq_heap->mode, freq_heap->status_bits,
+                    freq_heap->payload_data_off, accum_time, reqd_exposure, integ_num);
+            }
             /* If we have accumulated for long enough, write vectors to output block */
-            if(accum_time >= reqd_exposure)
+            // if(accum_time >= reqd_exposure)
+            if (freq_heap->spectrum_cntr >= end_exposure_spectrum_number)
             {
 #if 0            
                 // DEBUG status of switching signals
@@ -395,15 +435,17 @@ void vegas_accum_thread(void *_args) {
                                        accum_dirty, accumulator, data_cols, &sf, &blkstats);
                 accum_time = 0;
                 integ_num += 1;
+                end_exposure_spectrum_number = freq_heap->spectrum_cntr + spectra_per_exposure;
+                
                 if (use_scanlen)
                 {
-                    if (check_scan_length(raw_time_counter, fpgafreq, scan_length_seconds))
+                    if (check_scan_length(&clock, full_time_counter, fpgafreq, scan_length_seconds))
                     {
                         // set the incomplete block as filled so that the next stage
                         // sees the partial block. num_datasets indicates the amount of 
                         // partial data.     
                         printf("hpc detected end of scan\n"); 
-                        flush_end_of_scan(db_out, &curblock_out);
+                        flush_end_of_scan(db_out, &curblock_out, db_in, curblock_in);
                         pthread_exit(0);
                     }
                 }
@@ -493,6 +535,7 @@ void vegas_accum_thread(void *_args) {
         blkstats.npacket += gp.num_pkts_rcvd;
         blkstats.n_pkt_drop += gp.num_pkts_dropped;
 
+        
         /* Done with current input block */
         vegas_databuf_set_free(db_in, curblock_in);
         curblock_in = (curblock_in + 1) % db_in->n_block;
@@ -511,28 +554,23 @@ void vegas_accum_thread(void *_args) {
     pthread_cleanup_pop(0); /* Closes vegas_databuf_detach */
 }
 
-int check_scan_length(int64_t time_counter, double fpgafreq, double scanlen)
+uint64_t get_full_range_counter(struct Clock *clock, uint64_t raw_time_counter)
 {
-    static int64_t last_time_counter = 0;
-    static int64_t upper_bits = 0;
-    double scan_length_clock;
-    int64_t scanlen_time_counter;
-    
-    if (fpgafreq < 0.1)
-    {
-        // I hate to do this, but this is a signal to reset our static vars
-        last_time_counter = 0;
-        upper_bits = 0;
-        return 0;
-    }
-    
-    if (last_time_counter > time_counter)
+    if (clock->last_time_counter > raw_time_counter)
     {
         /* 40 bit time counter has rolled over */
-        upper_bits += 1LL<<40;   
+        clock->upper_bits += 1LL<<40;   
     }
-    last_time_counter = time_counter;
-    scanlen_time_counter = upper_bits + time_counter;
+    clock->last_time_counter = raw_time_counter;
+    return (clock->upper_bits + raw_time_counter);       
+}
+
+int check_scan_length(struct Clock *clock, int64_t time_counter, double fpgafreq, double scanlen)
+{
+    double scan_length_clock;
+    uint64_t scanlen_time_counter;
+    
+    scanlen_time_counter = get_full_range_counter(clock, time_counter);
     /* check if scan length has been reached */
     scan_length_clock = (double)(scanlen_time_counter) / fpgafreq;
 
@@ -554,7 +592,6 @@ void write_full_integration(struct vegas_databuf *db_out, int *cur_block_out,
     struct databuf_index* index_out;
     int curblock_out = *cur_block_out;
     char *hdr_out = vegas_databuf_header(db_out, curblock_out);
-    char *hdr_in  = vegas_databuf_header(db_in, curblock_in);
     int struct_offset, array_offset;
     
     int rv = 0;
@@ -647,12 +684,17 @@ void write_full_integration(struct vegas_databuf *db_out, int *cur_block_out,
     *cur_block_out = curblock_out;
 }
 
-void flush_end_of_scan(struct vegas_databuf *db_out, int *cur_block_out)
+void flush_end_of_scan(struct vegas_databuf *db_out, int *cur_block_out, struct vegas_databuf *db_in, int curblock_in)
 {
     int curblock_out = *cur_block_out;
     struct databuf_index* index_out;
     struct sdfits_data_columns data_cols;
     int rv;
+    int array_size;
+    char *hdr_out;
+    
+    index_out = (struct databuf_index*)vegas_databuf_index(db_out, curblock_out);
+    array_size = index_out->array_size;
         
     // flush the current buffer
     vegas_databuf_set_filled(db_out, curblock_out);
@@ -671,10 +713,20 @@ void flush_end_of_scan(struct vegas_databuf *db_out, int *cur_block_out)
             break;
         }
     }
+        
     memset(&data_cols, 0, sizeof(data_cols));
     /* Initialise the index in new output block */
+    // Copy the status header from the input, so that the dummy block has a legal header
+    hdr_out = vegas_databuf_header(db_out, curblock_out);
+    memcpy(hdr_out, vegas_databuf_header(db_in, curblock_in), VEGAS_STATUS_SIZE);
+    
     index_out = (struct databuf_index*)vegas_databuf_index(db_out, curblock_out);
     index_out->num_datasets = 1;
+    index_out->array_size = array_size;
+    index_out->disk_buf[0].struct_offset = 0;
+    index_out->disk_buf[0].array_offset = 0;
+    
+    
     data_cols.integ_num = -1;                
     memcpy(vegas_databuf_data(db_out, curblock_out),
            &data_cols, sizeof(data_cols));          
