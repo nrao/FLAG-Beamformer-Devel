8ad4e5e5d61b92712e005dd951a225b96aec3782
Thomas Chamberlin <tchamber@nrao.edu>
2015-05-19 17:07:50 -0400

    Changed bin size

--- src/vegas_hpc/src/vegas_databuf.h
+++ src/vegas_hpc/src/vegas_databuf.h
@@ -32,7 +32,7 @@ struct decprecated_vegas_databuf {
 // The bin size is the number of elements in the lower trianglular
 //   portion of the covariance matrix
 //   (41 * 20) gives us the number of complex pair elements
-#define BIN_SIZE (41 * 20)
+#define BIN_SIZE (65 * 32)
 // #define BIN_SIZE 4
 // This is the number of frequency channels that we will be correlating
 //   It will be either 5, 50, or 160, and probably should always be a macro
