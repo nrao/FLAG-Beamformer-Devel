404e1b14085003dd61e22a9a4d7af5d8dd2ffc1d
Thomas Chamberlin <tchamber@nrao.edu>
2015-06-22 09:48:12 -0400

    Minor changes to matrix.py. Added some comments and refactored for clarity

--- scripts/matrix.py
+++ scripts/matrix.py
@@ -1,89 +1,84 @@
-# N = 8
-# M = 8
-
-# n = N/2
-# m = M/2
-
+# Copyright (C) 2015 Associated Universities, Inc. Washington DC, USA.
 # 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# 
+# Correspondence concerning GBT software should be addressed as follows:
+#	GBT Operations
+#	National Radio Astronomy Observatory
+#	P. O. Box 2
+#	Green Bank, WV 24944-0002 USA
 
+#
+# matrix.py
+#
 
+N = 4
 
-# for dd in d:
-#     print dd
-#             
-
-
-
-# def get_sub_matrix(row, col):
-#   for xi in range(1, 3):
-#       for yi in range(1, 3):
-#           if xi != yi:
-#               print (xi + row*2, yi + col*2),
-#       print
-
-
-
-# print
-
-# redundant_blocks = []
-# next_red = 1
-# inc = 8
-# for i in range(820):
-#   if i == next_red:
-#       next_red += inc
-#       inc += 4
-#       redundant_blocks.append(i)
-#       print i
-
-
-# get_sub_matrix(1, 0)
-
-
-
-n = 4
-m = n
-
-# matrix = []
-
-# # for ni in range(1, n + 1):
-# #     for mi in range(1, ni + 1):
-# #         print (ni,mi)
-# d = []
-
-def get_input_coeff(ni, mi):
+# Given the coordinates of a "new" matrix block,
+#   return the coordinates that are within that block as a list of tuples
+def get_indices_in_block(n, m):
     d = []
-    d.append((2*ni,2*mi))
-    if mi != ni:
-        d.append((2*ni,2*mi+1))
-    d.append((2*ni+1,2*mi))
-    d.append((2*ni+1,2*mi+1))
+    d.append((2*n,2*m))
+    # Redundant elements occur only within the blocks along the
+    #   diagonal of the matrix. We know that only blocks along
+    #   the diagonal will have n == m
+    if m != n:
+        d.append((2*n,2*m+1))
+    d.append((2*n+1,2*m))
+    d.append((2*n+1,2*m+1))
     return d
 
-# for dd in range(d)
-
+# Given the coordinates of an element in a "new"
+#   matrix block, return the index in the "old"
+#   array that contains the element
 def get_old_index(n, m):
     old_index = 0
     for i in range(n , 0, -1):
         old_index += i
 
     old_index += m
-    print "old_index for (%d, %d): %d" % (n, m, old_index)
     return old_index
 
+# Given the coordinates of a block in the "new"
+#   lower triangular matrix,
+#   return the indices of the "old" array that
+#   map to the data in the block
 def get_old_indices(n, m):
-    indices = []
-    for dd in get_input_coeff(n, m):
-        # print dd
-        indices.append(get_old_index(dd[0], dd[1]))
+    old_indices = []
+    # Get the indices of the elements contained within the
+    #   "new" matrix block
+    block_indices = get_indices_in_block(n, m) 
+    for block_index in block_indices:
+        old_indices.append(get_old_index(block_index[0], block_index[1]))
 
-    return indices
+    return old_indices
 
+# The "new" lower triangular matrix exists as a series
+#   of blocks. Some contain 4 elements, others only contain
+#   3 due to eliminated redundancies.
+# The coordinates in these blocks can be mapped onto the data
+#   existing in the "old" array of data stored in the "old" FITS
+#   file.
+
+# So, we loop through all of the blocks in the "new" matrix.
 bf_cov = []
-for i in range(m):
+for i in range(N):
     for j in range(i + 1):
+    	# For each block we get the indices of the "old" array that contain the data we are looking for.
         bf_cov.extend(get_old_indices(i, j))
 
+# Verify output
 for block in bf_cov:
-    print block, " ", 
-
-# get_old_indices(2,0)
+    print block, 
